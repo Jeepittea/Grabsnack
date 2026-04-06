95d5635c11fc8fe5df790834530250b7a3833f1d
Justin Wolfe <your_email@gmail.com>
2026-04-06 17:47:03 +0800

    Removed sensitive OAuth credentials

---
+++ grabsnack-backend/src/main/java/com/grabsnack/backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.grabsnack.backend.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        filterChain.doFilter(request,response);
+    }
+
+}
